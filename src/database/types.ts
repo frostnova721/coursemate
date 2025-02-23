7b94e51e7dfadf5ad2e963a2dc125cd7c24222c6
Rizzy <hrisheekeshpv@gmail.com>
2025-02-23 16:03:10 +0530

    add middlewares, jwt

--- src/database/types.ts
+++ src/database/types.ts
@@ -16,3 +16,11 @@ export interface IUser {
     email?: string;
     isAdmin?: boolean;
 }
+
+// Payload for JWT
+export interface IUserPayload {
+    id: number;
+    fname: string;
+    lname: string;
+    isAdmin: boolean;
+}
---
+++ src/middlewares/tokenVerifier.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { IUserPayload } from "../database/types";
+import { ACCESS_KEY } from "../utils/constants";
+
+
+interface AuthRequest extends Request {
+  user?: IUserPayload;
+}
+
+// middle ware func to verify the token
+const verifyToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  try {
+    const dec = jwt.verify(token, ACCESS_KEY) as AuthRequest["user"];
+    req.body.user = dec;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+export { verifyToken };
